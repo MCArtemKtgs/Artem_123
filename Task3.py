001d622a8fdbc94c36e4cbac3a4c50e93226a740
MCArtemKtgs <166371121+MCArtemKtgs@users.noreply.github.com>
2024-04-26 11:00:59 +0300

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,23 @@
+# Основные действия с данными в Python
+
+a = 1, 2, 3
+b = str(a)
+print(b)
+print(type(b))
+
+a = 1, 5.5, "hello"
+b = list(a)
+print(b)
+print(type(b))
+
+
+# a = int(input('Введите число: '))
+# if a % 2 == 0:
+#     print(f'Число {a} является нечетным')
+# else:
+#     print(f'Число {a} является четным')
+
+
+a = str(input('Введите строку: '))
+b = len(a)
+print(f'Количество символов в строке: {b}')
